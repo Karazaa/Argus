408ebda28cf20daad34c8f1aa73288f5b79631b9
Karazaa <jamescolrepo@gmail.com>
2024-02-28 20:47:57 -0800

    Starting the process of writing a C# plugin code generator for Argus that can be executed via the Unreal Build Tool.

---
+++ Argus/Plugins/ArgusCodeGenerator/ArgusCPPGenerator.cs
@@ -0,0 +1,28 @@
+﻿using EpicGames.Core;
+using EpicGames.UHT.Types;
+using EpicGames.UHT.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ArgusCodeGenerator
+{
+    internal class ArgusCPPGenerator : ScriptCodeGeneratorBase
+    {
+        public ArgusCPPGenerator(IUhtExportFactory factory) : base(factory)
+        {
+        }
+
+        protected override void ExportClass(StringBuilder builder, UhtClass classObj)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Finish(StringBuilder builder, List<UhtClass> classes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Argus/Plugins/ArgusCodeGenerator/ArgusCodeGenerator.cs
@@ -0,0 +1,25 @@
+﻿using EpicGames.UHT.Tables;
+using EpicGames.UHT.Utils;
+
+namespace ArgusCodeGenerator
+{
+    [UnrealHeaderTool]
+    public class ArgusCodeGenerator
+    {
+        [UhtExporter(Name = "ArgusCodeGenerator", Description = "Code generation for the Argus project", Options = UhtExporterOptions.Default, ModuleName = "ArgusCodeGenerator")]
+        private static void ScriptGeneratorExporter(IUhtExportFactory Factory)
+        {
+            // Make sure this plugin should be run
+            if (!Factory.Session.IsPluginEnabled("ArgusCodeGenerator", false))
+            {
+                return;
+            }
+
+            // Based on the WITH_LUA setting, run the proper exporter.
+            if (Factory.PluginModule != null)
+            {
+                new ArgusCPPGenerator(Factory).Generate();
+            }
+        }
+    }
+}
