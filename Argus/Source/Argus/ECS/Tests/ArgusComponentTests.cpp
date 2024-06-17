26fb8837cc677a7775f835fd84f198bbef108076
James Collins <jamescolrepo@gmail.com>
2024-06-16 22:13:17 -0700

    Adding more formatting and pragma regions to tests. Still have to get to ArgusEntityTests and TransformSystemsTests.

--- Argus/Source/Argus/ECS/Tests/ArgusComponentTests.cpp
+++ Argus/Source/Argus/ECS/Tests/ArgusComponentTests.cpp
@@ -24,7 +24,7 @@ bool ArgusComponentHealthComponentPersistenceTest::RunTest(const FString& Parame
 	healthComponent->m_health = expectedSetHealthValue;
 	healthComponent = entity.GetComponent<HealthComponent>();
 
-#pragma region
+#pragma region Test HealthComponent setting health value.
 	TestEqual
 	(
 		FString::Printf(TEXT("[%s] Creating a %s, setting it to %d, then checking the value is %d on retrieval."), ARGUS_FUNCNAME, ARGUS_NAMEOF(HealthComponent), expectedSetHealthValue, expectedSetHealthValue), 
@@ -35,7 +35,7 @@ bool ArgusComponentHealthComponentPersistenceTest::RunTest(const FString& Parame
 
 	*healthComponent = HealthComponent();
 
-#pragma region
+#pragma region Test that resetting a HealthComponent returns to the default value.
 	TestEqual
 	(
 		FString::Printf(TEXT("[%s] Creating a %s, setting it to %d, resetting it, then checking the value is %d after reset."), ARGUS_FUNCNAME, ARGUS_NAMEOF(HealthComponent), expectedSetHealthValue, expectedPostResetHealthValue), 
@@ -61,7 +61,7 @@ bool ArgusComponentTargetingComponentHasEntityTargetTest::RunTest(const FString&
 		return false;
 	}
 
-#pragma region
+#pragma region Test creating a new TargetingComponent and it not having an entity target by default.
 	TestFalse
 	(
 		FString::Printf(TEXT("[%s] Creating a %s and confirming that it does not have an entity target."), ARGUS_FUNCNAME, ARGUS_NAMEOF(TargetingComponent)),
@@ -71,7 +71,7 @@ bool ArgusComponentTargetingComponentHasEntityTargetTest::RunTest(const FString&
 	
 	targetingComponent->m_targetEntityId = entity2.GetId();
 
-#pragma region
+#pragma region Test setting target entity.
 	TestTrue
 	(
 		FString::Printf(TEXT("[%s] Creating a %s, setting target to another entity, and confirming that it does have a target."), ARGUS_FUNCNAME, ARGUS_NAMEOF(TargetingComponent)),
@@ -79,7 +79,7 @@ bool ArgusComponentTargetingComponentHasEntityTargetTest::RunTest(const FString&
 	);
 #pragma endregion
 
-#pragma region
+#pragma region Test setting target entity to a different entity.
 	TestEqual
 	(
 		FString::Printf(TEXT("[%s] Creating a %s, setting target to another entity, then checking the value is the right ID."), ARGUS_FUNCNAME, ARGUS_NAMEOF(TargetingComponent)), 
@@ -100,7 +100,7 @@ bool ArgusComponentTargetingComponentHasLocationTargetTest::RunTest(const FStrin
 	TargetingComponent* targetingComponent = entity.AddComponent<TargetingComponent>();
 	const FVector targetLocation = FVector(10.0f, 10.0f, 10.0f);
 
-#pragma region
+#pragma region Test creating a TargetingComponent and validating that it does not have a location target by default.
 	TestFalse
 	(
 		FString::Printf(TEXT("[%s] Creating a %s and confirming that it does not have a location target."), ARGUS_FUNCNAME, ARGUS_NAMEOF(TargetingComponent)), 
@@ -110,7 +110,7 @@ bool ArgusComponentTargetingComponentHasLocationTargetTest::RunTest(const FStrin
 
 	targetingComponent->m_targetLocation = targetLocation;
 
-#pragma region
+#pragma region Test setting location target.
 	TestTrue
 	(
 		FString::Printf(TEXT("[%s] Creating a %s, setting target to a location, and confirming that it does have a target."), ARGUS_FUNCNAME, ARGUS_NAMEOF(TargetingComponent)),
@@ -118,7 +118,7 @@ bool ArgusComponentTargetingComponentHasLocationTargetTest::RunTest(const FStrin
 	);
 #pragma endregion
 
-#pragma region
+#pragma region Test setting target location to a different location.
 	TestEqual
 	(
 		FString::Printf(TEXT("[%s] Creating a %s, setting target to a location, then checking the value is the right location."), ARGUS_FUNCNAME, ARGUS_NAMEOF(TargetingComponent)), 
@@ -146,7 +146,7 @@ bool ArgusComponentIdentityComponentIsInFactionMaskTest::RunTest(const FString& 
 	identityComponent->m_faction = EFaction::FactionA;
 	uint8 factionMask = 0u;
 
-#pragma region
+#pragma region Test creating an IdentityComponent of faction A and confirming that it is not present in an empty faction mask.
 	TestFalse
 	(
 		FString::Printf(TEXT("[%s] Creating an %s with %s and confirming that it is not present in an empty faction mask."), ARGUS_FUNCNAME, ARGUS_NAMEOF(IdentityComponent), ARGUS_NAMEOF(EFaction::FactionA)),
@@ -156,7 +156,7 @@ bool ArgusComponentIdentityComponentIsInFactionMaskTest::RunTest(const FString& 
 
 	factionMask = 0xFF;
 
-#pragma region
+#pragma region Test that the IdentityComponent's faction is present in a full faction mask.
 	TestTrue
 	(
 		FString::Printf(TEXT("[%s] Creating an %s with %s and confirming that it is present in a full faction mask."), ARGUS_FUNCNAME, ARGUS_NAMEOF(IdentityComponent), ARGUS_NAMEOF(EFaction::FactionA)),
@@ -187,7 +187,7 @@ bool ArgusComponentIdentityComponentAddAllyFactionTest::RunTest(const FString& P
 
 	identityComponent1->AddAllyFaction(EFaction::FactionB);
 
-#pragma region
+#pragma region Test creating an IdentityComponent of faction A, adding it as an ally of another IdentityComponent, and then validating that it is in the ally faction mask.
 	TestTrue
 	(
 		FString::Printf(TEXT("[%s] Creating two %ss, adding one as the ally of another, and then testing to make sure it is in the ally faction mask."), ARGUS_FUNCNAME, ARGUS_NAMEOF(IdentityComponent)),
@@ -198,7 +198,7 @@ bool ArgusComponentIdentityComponentAddAllyFactionTest::RunTest(const FString& P
 	identityComponent1->AddEnemyFaction(EFaction::FactionB);
 	identityComponent1->AddAllyFaction(EFaction::FactionB);
 
-#pragma region
+#pragma region Test adding enemy and then ally and making sure it is in the ally faction mask.
 	TestTrue
 	(
 		FString::Printf(TEXT("[%s] Creating two %ss, adding one as the enemy and then ally of another, and then testing to make sure it is in the ally faction mask."), ARGUS_FUNCNAME, ARGUS_NAMEOF(IdentityComponent)),
@@ -206,7 +206,7 @@ bool ArgusComponentIdentityComponentAddAllyFactionTest::RunTest(const FString& P
 	);
 #pragma endregion
 
-#pragma region
+#pragma region Test adding enemy then ally and making sure it is not in the enemy faction mask.
 	TestFalse
 	(
 		FString::Printf(TEXT("[%s] Creating two %ss, adding one as the enemy and then ally of another, and then testing to make sure it is not in the enemy faction mask."), ARGUS_FUNCNAME, ARGUS_NAMEOF(IdentityComponent)),
@@ -237,7 +237,7 @@ bool ArgusComponentIdentityComponentAddEnemyFactionTest::RunTest(const FString& 
 
 	identityComponent1->AddEnemyFaction(EFaction::FactionA);
 
-#pragma region
+#pragma region Test creating two IdentityComponents, adding one as the enemy of itself, and then validating that it is not in its own enemy mask.
 	TestFalse
 	(
 		FString::Printf(TEXT("[%s] Creating two %ss, adding one as the enemy of itself, and then testing to make sure it is not in the enemy mask."), ARGUS_FUNCNAME, ARGUS_NAMEOF(IdentityComponent)),
@@ -247,7 +247,7 @@ bool ArgusComponentIdentityComponentAddEnemyFactionTest::RunTest(const FString& 
 
 	identityComponent1->AddEnemyFaction(EFaction::FactionB);
 
-#pragma region
+#pragma region Test adding an enemy and then validating that it is in the enemy mask.
 	TestTrue
 	(
 		FString::Printf(TEXT("[%s] Creating two %ss, adding one as the enemy of another, and then testing to make sure it is in the enemy faction mask."), ARGUS_FUNCNAME, ARGUS_NAMEOF(IdentityComponent)),
@@ -258,7 +258,7 @@ bool ArgusComponentIdentityComponentAddEnemyFactionTest::RunTest(const FString& 
 	identityComponent1->AddAllyFaction(EFaction::FactionB);
 	identityComponent1->AddEnemyFaction(EFaction::FactionB);
 
-#pragma region
+#pragma region Test adding a different faction as enemy and validating presence in the enemy mask.
 	TestTrue
 	(
 		FString::Printf(TEXT("[%s] Creating two %ss, adding one as the ally and then enemy of another, and then testing to make sure it is in the enemy faction mask."), ARGUS_FUNCNAME, ARGUS_NAMEOF(IdentityComponent)),
@@ -266,7 +266,7 @@ bool ArgusComponentIdentityComponentAddEnemyFactionTest::RunTest(const FString& 
 	);
 #pragma endregion
 
-#pragma region
+#pragma region Test adding an ally and then that as an enemy and making sure the faction is no longer present in the ally mask.
 	TestFalse
 	(
 		FString::Printf(TEXT("[%s] Creating two %ss, adding one as the ally and then enemy of another, and then testing to make sure it is not in the ally faction mask."), ARGUS_FUNCNAME, ARGUS_NAMEOF(IdentityComponent)),
@@ -290,7 +290,7 @@ bool ArgusComponentTaskComponentIsExecutingMoveTaskTest::RunTest(const FString& 
 		return false;
 	}
 
-#pragma region
+#pragma region Test that None is not considered a move task.
 	TestFalse
 	(
 		FString::Printf(TEXT("[%s] Creating an %s, giving it a %s, and checking if default task is a move task."), ARGUS_FUNCNAME, ARGUS_NAMEOF(ArgusEntity), ARGUS_NAMEOF(TaskComponent)),
@@ -300,7 +300,7 @@ bool ArgusComponentTaskComponentIsExecutingMoveTaskTest::RunTest(const FString& 
 
 	taskComponent->m_currentTask = ETask::ProcessMoveToLocationCommand;
 
-#pragma region
+#pragma region Test that ProcessMoveToLocationCommand is not considered a move task.
 	TestFalse
 	(
 		FString::Printf(TEXT("[%s] Creating an %s, giving it a %s, setting task to %s, and checking if that is considered a move task."), ARGUS_FUNCNAME, ARGUS_NAMEOF(ArgusEntity), ARGUS_NAMEOF(TaskComponent), ARGUS_NAMEOF(ETask::ProcessMoveToLocationCommand)),
@@ -310,7 +310,7 @@ bool ArgusComponentTaskComponentIsExecutingMoveTaskTest::RunTest(const FString& 
 
 	taskComponent->m_currentTask = ETask::ProcessMoveToEntityCommand;
 
-#pragma region
+#pragma region Test that ProcessMoveToEntityCommand is not considered a move task.
 	TestFalse
 	(
 		FString::Printf(TEXT("[%s] Creating an %s, giving it a %s, setting task to %s, and checking if that is considered a move task."), ARGUS_FUNCNAME, ARGUS_NAMEOF(ArgusEntity), ARGUS_NAMEOF(TaskComponent), ARGUS_NAMEOF(ETask::ProcessMoveToEntityCommand)),
@@ -320,7 +320,7 @@ bool ArgusComponentTaskComponentIsExecutingMoveTaskTest::RunTest(const FString& 
 
 	taskComponent->m_currentTask = ETask::FailedToFindPath;
 
-#pragma region
+#pragma region Test that FailedToFindPath is not considered a move task.
 	TestFalse
 	(
 		FString::Printf(TEXT("[%s] Creating an %s, giving it a %s, setting task to %s, and checking if that is considered a move task."), ARGUS_FUNCNAME, ARGUS_NAMEOF(ArgusEntity), ARGUS_NAMEOF(TaskComponent), ARGUS_NAMEOF(ETask::FailedToFindPath)),
@@ -330,7 +330,7 @@ bool ArgusComponentTaskComponentIsExecutingMoveTaskTest::RunTest(const FString& 
 
 	taskComponent->m_currentTask = ETask::MoveToLocation;
 
-#pragma region
+#pragma region Test that MoveToLocation is considered a move task.
 	TestTrue
 	(
 		FString::Printf(TEXT("[%s] Creating an %s, giving it a %s, setting task to %s, and validating that it is considered a move task."), ARGUS_FUNCNAME, ARGUS_NAMEOF(ArgusEntity), ARGUS_NAMEOF(TaskComponent), ARGUS_NAMEOF(ETask::MoveToLocation)),
@@ -340,7 +340,7 @@ bool ArgusComponentTaskComponentIsExecutingMoveTaskTest::RunTest(const FString& 
 
 	taskComponent->m_currentTask = ETask::MoveToEntity;
 
-#pragma region
+#pragma region Test that MoveToEntity is considered a move task.
 	TestTrue
 	(
 		FString::Printf(TEXT("[%s] Creating an %s, giving it a %s, setting task to %s, and validating that it is considered a move task."), ARGUS_FUNCNAME, ARGUS_NAMEOF(ArgusEntity), ARGUS_NAMEOF(TaskComponent), ARGUS_NAMEOF(ETask::MoveToEntity)),
--- Argus/Source/Argus/ECS/Tests/ArgusEntityTemplateTests.cpp
+++ Argus/Source/Argus/ECS/Tests/ArgusEntityTemplateTests.cpp
@@ -15,11 +15,13 @@ bool ArgusEntityTemplateInstantiateEntityTest::RunTest(const FString& Parameters
 
 	const uint16 targetId = static_cast<uint16>(UEntityPriority::MediumPriority);
 
+#pragma region Test that there is not an entity of a specific entityID
 	TestFalse
 	(
 		FString::Printf(TEXT("[%s] Validating that there isn't an entity yet."), ARGUS_FUNCNAME),
 		ArgusEntity::DoesEntityExist(targetId)
 	);
+#pragma endregion
 
 	UHealthComponentData* healthComponentData = NewObject<UHealthComponentData>();
 	healthComponentData->m_health = expectedHealthValue;
@@ -30,20 +32,24 @@ bool ArgusEntityTemplateInstantiateEntityTest::RunTest(const FString& Parameters
 
 	ArgusEntity newEntity = entityTemplate->MakeEntity();
 
+#pragma region Test that an entity has been successfully created from an entity template.
 	TestTrue
 	(
 		FString::Printf(TEXT("[%s] Validating that an %s has been created."), ARGUS_FUNCNAME, ARGUS_NAMEOF(ArgusEntity)),
 		ArgusEntity::DoesEntityExist(targetId)
 	);
+#pragma endregion
 
 	uint32 healthValue = newEntity.GetComponent<HealthComponent>()->m_health;
 
+#pragma region Test that a health component exists on the new entity with a specific health value.
 	TestEqual
 	(
 		FString::Printf(TEXT("[%s] Validating that a %s exists with the proper value, %d."), ARGUS_FUNCNAME, ARGUS_NAMEOF(HealthComponent), expectedHealthValue),
 		healthValue,
 		expectedHealthValue
 	);
+#pragma endregion
 
 	return true;
 }
