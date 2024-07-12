d3c57d6baa4d1714f27455f866ee0146e3d934e8
James Collins <jamescolrepo@gmail.com>
2024-07-11 21:26:38 -0700

    Wrote a functional test for RetrievingArgusActorFromEntity. Close #53

--- Argus/Source/Argus/FunctionalTesting/RetrieveArgusActorFromEntityFunctionalTest.cpp
+++ Argus/Source/Argus/FunctionalTesting/RetrieveArgusActorFromEntityFunctionalTest.cpp
@@ -1,3 +1,93 @@
 // Copyright Karazaa. This is a part of an RTS project called Argus.
 
 #include "RetrieveArgusActorFromEntityFunctionalTest.h"
+#include "ArgusActor.h"
+#include "ArgusGameInstance.h"
+#include "ArgusLogging.h"
+#include "ArgusMacros.h"
+
+ARetrieveArgusActorFromEntityFunctionalTest::ARetrieveArgusActorFromEntityFunctionalTest(const FObjectInitializer& ObjectInitializer)
+{
+	TestLabel = k_testName;
+}
+
+bool ARetrieveArgusActorFromEntityFunctionalTest::DidArgusFunctionalTestFail()
+{
+	if (!m_argusActorToFind.IsValid())
+	{
+		m_testFailedMessage = FString::Printf
+		(
+			TEXT("[%s] Failed test due to invalid setup. %s must be a valid reference."),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(m_argusActorToFind)
+		);
+		return true;
+	}
+
+	return false;
+}
+
+void ARetrieveArgusActorFromEntityFunctionalTest::StartNextTestStep()
+{
+	switch (m_testStepIndex)
+	{
+	case 0:
+		StartRetrieveArgusActorFromEntityTestStep();
+		break;
+	default:
+		break;
+	}
+}
+
+bool ARetrieveArgusActorFromEntityFunctionalTest::DidCurrentTestStepSucceed()
+{
+	switch (m_testStepIndex)
+	{
+	case 0:
+		return DidRetrieveArgusActorFromEntityTestStepSucceed();
+	default:
+		break;
+	}
+	return false;
+}
+
+bool ARetrieveArgusActorFromEntityFunctionalTest::DidRetrieveArgusActorFromEntityTestStepSucceed()
+{
+	if (DidArgusFunctionalTestFail())
+	{
+		return false;
+	}
+
+	const UWorld* world = GetWorld();
+	if (!world)
+	{
+		return false;
+	}
+
+	const UArgusGameInstance* gameInstance = world->GetGameInstance<UArgusGameInstance>();
+	if (!gameInstance)
+	{
+		return false;
+	}
+
+	AArgusActor* retrievedActor = gameInstance->GetArgusActorFromArgusEntity(m_argusActorToFind->GetEntity());
+
+	if (retrievedActor)
+	{
+		m_testSucceededMessage = FString::Printf
+		(
+			TEXT("[%s] Successfully retrieving %s from an %s"),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(m_argusActorToFind),
+			ARGUS_NAMEOF(ArgusEntity)
+		);
+		return true;
+	}
+
+	return false;
+}
+
+void ARetrieveArgusActorFromEntityFunctionalTest::StartRetrieveArgusActorFromEntityTestStep()
+{
+	StartStep(TEXT("Test retrieve ArgusActor from ArgusEntity."));
+}
--- Argus/Source/Argus/FunctionalTesting/RetrieveArgusActorFromEntityFunctionalTest.h
+++ Argus/Source/Argus/FunctionalTesting/RetrieveArgusActorFromEntityFunctionalTest.h
@@ -3,3 +3,32 @@
 #pragma once
 
 #include "ArgusFunctionalTest.h"
+#include "RetrieveArgusActorFromEntityFunctionalTest.generated.h"
+
+class AArgusActor;
+
+UCLASS()
+class ARGUS_API ARetrieveArgusActorFromEntityFunctionalTest : public AArgusFunctionalTest
+{
+	GENERATED_BODY()
+
+private:
+	static constexpr const char* k_testName = "Argus.FunctionalTest.RetrieveArgusActorFromEntity";
+	static constexpr uint8       k_totalNumSteps = 1;
+
+public:
+	ARetrieveArgusActorFromEntityFunctionalTest(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	UPROPERTY(EditAnywhere)
+	TWeakObjectPtr<AArgusActor> m_argusActorToFind = nullptr;
+
+protected:
+	virtual bool    DidArgusFunctionalTestFail()	override;
+	virtual bool    DidCurrentTestStepSucceed()		override;
+	virtual void    StartNextTestStep()				override;
+	virtual uint8   GetTotalNumTestSteps()			override { return k_totalNumSteps; }
+
+private:
+	bool DidRetrieveArgusActorFromEntityTestStepSucceed();
+	void StartRetrieveArgusActorFromEntityTestStep();
+};
