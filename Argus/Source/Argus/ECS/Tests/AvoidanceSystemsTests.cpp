12533c1039e20a459ab3435ada8f30bf81acb6b7
James Collins <jamescolrepo@gmail.com>
2024-11-01 10:28:22 -0700

    Wrapping up avoidance systems tests. Close #74

--- Argus/Source/Argus/ECS/Tests/AvoidanceSystemsTests.cpp
+++ Argus/Source/Argus/ECS/Tests/AvoidanceSystemsTests.cpp
@@ -11,6 +11,14 @@
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(AvoidanceSystemsProcessORCAvoidanceTest, "Argus.ECS.Systems.AvoidanceSystems.ProcessORCAvoidance", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::SmokeFilter)
 bool AvoidanceSystemsProcessORCAvoidanceTest::RunTest(const FString& Parameters)
 {
+	const float deltaTime = 1.0f;
+	const float desiredSpeed = 100.0f;
+	const FVector velocity = FVector(100.0f, 0.0f, 0.0f);
+	const FVector secondVelocity = FVector(-100.0f, 0.0f, 0.0f);
+	const FVector secondEntityLocation = FVector(100.0f, 0.0f, 0.0f);
+	const FVector targetLocation = FVector(200.0f, 0.0f, 0.0f);
+	const FVector secondTargetLocation = FVector(-100.0f, 0.0f, 0.0f);
+
 	ArgusEntity::FlushAllEntities();
 
 	ArgusEntity entity = ArgusEntity::CreateEntity();
@@ -32,7 +40,7 @@ bool AvoidanceSystemsProcessORCAvoidanceTest::RunTest(const FString& Parameters)
 		return false;
 	}
 
-	components.m_transformComponent->m_desiredSpeedUnitsPerSecond = 100.0f;
+	components.m_transformComponent->m_desiredSpeedUnitsPerSecond = desiredSpeed;
 	identityComponent->m_team = ETeam::TeamA;
 	spatialPartitioningComponent->m_argusKDTree.RebuildKDTreeForAllArgusEntities();
 
@@ -51,7 +59,7 @@ bool AvoidanceSystemsProcessORCAvoidanceTest::RunTest(const FString& Parameters)
 	);
 #pragma endregion
 
-	AvoidanceSystems::ProcessORCAvoidance(dummyPointer, 1.0f, components);
+	AvoidanceSystems::ProcessORCAvoidance(dummyPointer, deltaTime, components);
 
 #pragma region Test that a lone entity with no velocity would have a proposed avoidance velocity of 0 after running ProcessORCAvoidance
 	TestEqual
@@ -72,8 +80,72 @@ bool AvoidanceSystemsProcessORCAvoidanceTest::RunTest(const FString& Parameters)
 	ArgusEntity secondEntity = ArgusEntity::CreateEntity();
 	TransformSystems::TransformSystemsComponentArgs secondComponents;
 	secondComponents.m_entity = secondEntity;
-	secondComponents.m_taskComponent = secondEntity.AddComponent<TaskComponent>();
 	secondComponents.m_transformComponent = secondEntity.AddComponent<TransformComponent>();
+	if (!secondComponents.m_transformComponent)
+	{
+		return false;
+	}
+	secondComponents.m_transformComponent->m_transform.SetLocation(secondEntityLocation);
+
+	components.m_navigationComponent->m_navigationPoints.push_back(components.m_transformComponent->m_transform.GetLocation());
+	components.m_navigationComponent->m_navigationPoints.push_back(targetLocation);
+	components.m_taskComponent->m_currentTask = ETask::MoveToLocation;
+	components.m_transformComponent->m_currentVelocity = velocity;
+
+	spatialPartitioningComponent->m_argusKDTree.RebuildKDTreeForAllArgusEntities();
+	AvoidanceSystems::ProcessORCAvoidance(dummyPointer, deltaTime, components);
+	AvoidanceSystems::ProcessORCAvoidance(dummyPointer, deltaTime, secondComponents);
+
+#pragma region Test that a moving entity would not maintain its velocity when about to collide with a static entity.
+	TestNotEqual
+	(
+		FString::Printf
+		(
+			TEXT("[%s] Test that a moving %s would not maintain the same %s after calling %s when about to collide with a static %s."),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(ArgusEntity),
+			ARGUS_NAMEOF(m_proposedAvoidanceVelocity),
+			ARGUS_NAMEOF(AvoidanceSystems::ProcessORCAvoidance),
+			ARGUS_NAMEOF(ArgusEntity)
+		),
+		components.m_transformComponent->m_proposedAvoidanceVelocity,
+		components.m_transformComponent->m_currentVelocity
+	);
+#pragma endregion
+
+#pragma region Test that a moving entity would not stop when about to collide with a static entity.
+	TestNotEqual
+	(
+		FString::Printf
+		(
+			TEXT("[%s] Test that a moving %s would not stop after calling %s when about to collide with a static %s."),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(ArgusEntity),
+			ARGUS_NAMEOF(AvoidanceSystems::ProcessORCAvoidance),
+			ARGUS_NAMEOF(ArgusEntity)
+		),
+		components.m_transformComponent->m_proposedAvoidanceVelocity,
+		FVector::ZeroVector
+	);
+#pragma endregion
+
+#pragma region Test that a static entity would not move when a moving entity is about to collide with it.
+	TestEqual
+	(
+		FString::Printf
+		(
+			TEXT("[%s] Test that a static %s would not move to avoid a moving %s that is about to collide with it after calling %s."),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(ArgusEntity),
+			ARGUS_NAMEOF(ArgusEntity),
+			ARGUS_NAMEOF(AvoidanceSystems::ProcessORCAvoidance)
+		),
+		secondComponents.m_transformComponent->m_proposedAvoidanceVelocity,
+		FVector::ZeroVector
+	);
+#pragma endregion
+
+	secondComponents.m_taskComponent = secondEntity.AddComponent<TaskComponent>();
 	secondComponents.m_navigationComponent = secondEntity.AddComponent<NavigationComponent>();
 	secondComponents.m_targetingComponent = secondEntity.AddComponent<TargetingComponent>();
 	IdentityComponent* secondIdentityComponent = secondEntity.AddComponent<IdentityComponent>();
@@ -82,18 +154,10 @@ bool AvoidanceSystemsProcessORCAvoidanceTest::RunTest(const FString& Parameters)
 	{
 		return false;
 	}
-
-	secondComponents.m_transformComponent->m_transform.SetLocation(FVector(100.0f, 0.0f, 0.0f));
 	secondIdentityComponent->m_team = ETeam::TeamA;
 
-	components.m_navigationComponent->m_navigationPoints.push_back(FVector::ZeroVector);
-	components.m_navigationComponent->m_navigationPoints.push_back(FVector(200.0f, 0.0f, 0.0f));
-	components.m_taskComponent->m_currentTask = ETask::MoveToLocation;
-	components.m_transformComponent->m_currentVelocity = FVector(100.0f, 0.0f, 0.0f);
-
-	spatialPartitioningComponent->m_argusKDTree.RebuildKDTreeForAllArgusEntities();
-	AvoidanceSystems::ProcessORCAvoidance(dummyPointer, 1.0f, components);
-	AvoidanceSystems::ProcessORCAvoidance(dummyPointer, 1.0f, secondComponents);
+	AvoidanceSystems::ProcessORCAvoidance(dummyPointer, deltaTime, components);
+	AvoidanceSystems::ProcessORCAvoidance(dummyPointer, deltaTime, secondComponents);
 
 #pragma region Test that a moving entity would maintain its velocity when about to collide with a static but movable entity.
 	TestEqual
@@ -128,7 +192,82 @@ bool AvoidanceSystemsProcessORCAvoidanceTest::RunTest(const FString& Parameters)
 	);
 #pragma endregion
 
+	secondComponents.m_navigationComponent->m_navigationPoints.push_back(secondComponents.m_transformComponent->m_transform.GetLocation());
+	secondComponents.m_navigationComponent->m_navigationPoints.push_back(secondTargetLocation);
+	secondComponents.m_taskComponent->m_currentTask = ETask::MoveToLocation;
+	secondComponents.m_transformComponent->m_currentVelocity = secondVelocity;
+	secondComponents.m_transformComponent->m_desiredSpeedUnitsPerSecond = desiredSpeed;
+
+	AvoidanceSystems::ProcessORCAvoidance(dummyPointer, deltaTime, components);
+	AvoidanceSystems::ProcessORCAvoidance(dummyPointer, deltaTime, secondComponents);
+
 	return true;
+
+#pragma region Test that a moving entity would not maintain its velocity when about to collide with a static entity.
+	TestNotEqual
+	(
+		FString::Printf
+		(
+			TEXT("[%s] Test that a moving %s would not maintain the same %s after calling %s when about to collide with another moving %s."),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(ArgusEntity),
+			ARGUS_NAMEOF(m_proposedAvoidanceVelocity),
+			ARGUS_NAMEOF(AvoidanceSystems::ProcessORCAvoidance),
+			ARGUS_NAMEOF(ArgusEntity)
+		),
+		components.m_transformComponent->m_proposedAvoidanceVelocity,
+		components.m_transformComponent->m_currentVelocity
+	);
+#pragma endregion
+
+#pragma region Test that a moving entity would not stop when about to collide with a moving entity.
+	TestNotEqual
+	(
+		FString::Printf
+		(
+			TEXT("[%s] Test that a moving %s would not stop after calling %s when about to collide with a moving %s."),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(ArgusEntity),
+			ARGUS_NAMEOF(AvoidanceSystems::ProcessORCAvoidance),
+			ARGUS_NAMEOF(ArgusEntity)
+		),
+		components.m_transformComponent->m_proposedAvoidanceVelocity,
+		FVector::ZeroVector
+	);
+#pragma endregion
+
+#pragma region Test that the other moving entity would not maintain its velocity when about to collide with the original moving entity.
+	TestNotEqual
+	(
+		FString::Printf
+		(
+			TEXT("[%s] Test that the other moving %s would not maintain the same %s after calling %s when about to collide with a the original moving %s."),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(ArgusEntity),
+			ARGUS_NAMEOF(m_proposedAvoidanceVelocity),
+			ARGUS_NAMEOF(AvoidanceSystems::ProcessORCAvoidance),
+			ARGUS_NAMEOF(ArgusEntity)
+		),
+		secondComponents.m_transformComponent->m_proposedAvoidanceVelocity,
+		secondComponents.m_transformComponent->m_currentVelocity
+	);
+#pragma endregion
+
+#pragma region Test that the other moving entity would not stop when about to collide with the original moving entity.
+	TestNotEqual
+	(
+		FString::Printf
+		(
+			TEXT("[%s] Test that the other moving %s would not stop after calling %s when about to collide with the other moving %s."),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(ArgusEntity),
+			ARGUS_NAMEOF(AvoidanceSystems::ProcessORCAvoidance),
+			ARGUS_NAMEOF(ArgusEntity)
+		),
+		secondComponents.m_transformComponent->m_proposedAvoidanceVelocity,
+		FVector::ZeroVector
+	);
+#pragma endregion
 }
 
 #endif //WITH_AUTOMATION_TESTS
