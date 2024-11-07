06c73c7416e9a59d064d51213669b53ace33437f
James Collins <jamescolrepo@gmail.com>
2024-11-07 08:49:03 -0800

    Wrapping up ArgusMathTests with an FVector test for the ExponentialDecaySmoother.

--- Argus/Source/Argus/Utilities/Tests/ArgusMathTests.cpp
+++ Argus/Source/Argus/Utilities/Tests/ArgusMathTests.cpp
@@ -7,18 +7,65 @@
 
 #if WITH_AUTOMATION_TESTS
 
+namespace ArgusMathTests
+{
+	static constexpr int   k_sampleSize = 60;
+	static constexpr float k_framerate = 60.0f;
+	static constexpr float k_deltaTime = 1.0f / k_framerate;
+	static constexpr float k_valueTolerance = 0.001f;
+}
+
+void ChaseValue(ArgusMath::ExponentialDecaySmoother<float>& smoother, float from, float to)
+{
+	if (CVarEnableVerboseTestLogging.GetValueOnAnyThread())
+	{
+		UE_LOG(ArgusTestingLog, Display, TEXT("Smoothing from %f to %f"), from, to);
+	}
+	for (int i = 0; i < ArgusMathTests::k_sampleSize; ++i)
+	{
+		smoother.SmoothChase(to, ArgusMathTests::k_deltaTime);
+		if (CVarEnableVerboseTestLogging.GetValueOnAnyThread())
+		{
+			UE_LOG(ArgusTestingLog, Display, TEXT("%s value: %f"), ARGUS_NAMEOF(ExponentialDecaySmoother<float>), smoother.GetValue());
+		}
+	}
+}
+
+void ChaseValue(ArgusMath::ExponentialDecaySmoother<FVector>& smoother, const FVector& from, const FVector& to)
+{
+	if (CVarEnableVerboseTestLogging.GetValueOnAnyThread())
+	{
+		UE_LOG
+		(
+			ArgusTestingLog, Display, TEXT("Smoothing from {%f, %f, %f} to {%f, %f, %f}."),
+			from.X, from.Y, from.Z,
+			to.X, to.Y, to.Z
+		);
+	}
+	for (int i = 0; i < ArgusMathTests::k_sampleSize; ++i)
+	{
+		smoother.SmoothChase(to, ArgusMathTests::k_deltaTime);
+		const FVector value = smoother.GetValue();
+		if (CVarEnableVerboseTestLogging.GetValueOnAnyThread())
+		{
+			UE_LOG
+			(
+				ArgusTestingLog,
+				Display,
+				TEXT("%s value: {%f, %f, %f}"), ARGUS_NAMEOF(ExponentialDecaySmoother<FVector>),
+				value.X, value.Y, value.Z
+			);
+		}
+	}
+}
+
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(ArgusUtilitiesArgusMathExponentialDecaySmootherFloatTest, "Argus.Utilities.ArgusMath.ExponentialDecaySmoother.FloatTest", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::SmokeFilter)
 bool ArgusUtilitiesArgusMathExponentialDecaySmootherFloatTest::RunTest(const FString& Parameters)
 {
-	const int sampleSize = 60;
-	const float framerate = 60.0f;
-	const float deltaTime = 1.0f / framerate;
-	const float valueTolerance = 0.001f;
 	const float value0 = 0.0f;
 	const float value1 = 1.0f;
 	const float value2 = 10.0f;
-
-	float decayConstant = 10.0f;
+	const float decayConstant = 10.0f;
 
 	ArgusMath::ExponentialDecaySmoother<float> testValue = ArgusMath::ExponentialDecaySmoother<float>(0.0f, decayConstant);
 
@@ -50,18 +97,7 @@ bool ArgusUtilitiesArgusMathExponentialDecaySmootherFloatTest::RunTest(const FSt
 	);
 #pragma endregion
 
-	if (CVarEnableVerboseTestLogging.GetValueOnAnyThread())
-	{
-		UE_LOG(ArgusTestingLog, Display, TEXT("Smoothing from %f to %f"), value0, value1);
-	}
-	for (int i = 0; i < sampleSize; ++i)
-	{
-		testValue.SmoothChase(value1, deltaTime);
-		if (CVarEnableVerboseTestLogging.GetValueOnAnyThread())
-		{
-			UE_LOG(ArgusTestingLog, Display, TEXT("%s value: %f"), ARGUS_NAMEOF(ExponentialDecaySmoother), testValue.GetValue());
-		}
-	}
+	ChaseValue(testValue, value0, value1);
 
 #pragma region Test that smoothing from 0 to 1 over 1 second at 60 fps moves value sufficiently close to 1.0.
 	TestTrue
@@ -73,10 +109,10 @@ bool ArgusUtilitiesArgusMathExponentialDecaySmootherFloatTest::RunTest(const FSt
 			ARGUS_NAMEOF(ArgusMath::ExponentialDecaySmoother<float>),
 			value0,
 			value1,
-			framerate,
+			ArgusMathTests::k_framerate,
 			value1
 		),
-		FMath::IsNearlyEqual(testValue.GetValue(), value1, valueTolerance)
+		FMath::IsNearlyEqual(testValue.GetValue(), value1, ArgusMathTests::k_valueTolerance)
 	);
 #pragma endregion
 
@@ -96,18 +132,7 @@ bool ArgusUtilitiesArgusMathExponentialDecaySmootherFloatTest::RunTest(const FSt
 	);
 #pragma endregion
 
-	if (CVarEnableVerboseTestLogging.GetValueOnAnyThread())
-	{
-		UE_LOG(ArgusTestingLog, Display, TEXT("Smoothing from %f to %f"), value0, value2);
-	}
-	for (int i = 0; i < sampleSize; ++i)
-	{
-		testValue.SmoothChase(value2, deltaTime);
-		if (CVarEnableVerboseTestLogging.GetValueOnAnyThread())
-		{
-			UE_LOG(ArgusTestingLog, Display, TEXT("%s value: %f"), ARGUS_NAMEOF(ExponentialDecaySmoother), testValue.GetValue());
-		}
-	}
+	ChaseValue(testValue, value0, value2);
 
 #pragma region Test that smoothing from 0 to 10 over 1 second at 60 fps moves value sufficiently close to 10.0.
 	TestTrue
@@ -119,25 +144,14 @@ bool ArgusUtilitiesArgusMathExponentialDecaySmootherFloatTest::RunTest(const FSt
 			ARGUS_NAMEOF(ArgusMath::ExponentialDecaySmoother<float>),
 			value0,
 			value2,
-			framerate,
+			ArgusMathTests::k_framerate,
 			value2
 		),
-		FMath::IsNearlyEqual(testValue.GetValue(), value2, valueTolerance)
+		FMath::IsNearlyEqual(testValue.GetValue(), value2, ArgusMathTests::k_valueTolerance)
 	);
 #pragma endregion
 
-	if (CVarEnableVerboseTestLogging.GetValueOnAnyThread())
-	{
-		UE_LOG(ArgusTestingLog, Display, TEXT("Smoothing from %f to %f"), value2, value0);
-	}
-	for (int i = 0; i < sampleSize; ++i)
-	{
-		testValue.SmoothChase(value0, deltaTime);
-		if (CVarEnableVerboseTestLogging.GetValueOnAnyThread())
-		{
-			UE_LOG(ArgusTestingLog, Display, TEXT("%s value: %f"), ARGUS_NAMEOF(ExponentialDecaySmoother), testValue.GetValue());
-		}
-	}
+	ChaseValue(testValue, value2, value0);
 
 #pragma region Test that smoothing from 10 to 0 over 1 second at 60 fps moves value sufficiently close to 0.0.
 	TestTrue
@@ -149,13 +163,110 @@ bool ArgusUtilitiesArgusMathExponentialDecaySmootherFloatTest::RunTest(const FSt
 			ARGUS_NAMEOF(ArgusMath::ExponentialDecaySmoother<float>),
 			value2,
 			value0,
-			framerate,
+			ArgusMathTests::k_framerate,
 			value0
 		),
-		FMath::IsNearlyEqual(testValue.GetValue(), value0, valueTolerance)
+		FMath::IsNearlyEqual(testValue.GetValue(), value0, ArgusMathTests::k_valueTolerance)
 	);
 #pragma endregion
 
 	return true;
 }
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(ArgusUtilitiesArgusMathExponentialDecaySmootherFVectorTest, "Argus.Utilities.ArgusMath.ExponentialDecaySmoother.FVectorTest", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::SmokeFilter)
+bool ArgusUtilitiesArgusMathExponentialDecaySmootherFVectorTest::RunTest(const FString& Parameters)
+{
+	const FVector vector0 = FVector::ZeroVector;
+	const FVector vector1 = FVector(100.0f, 50.0f, 80.0f);
+	const FVector vector2 = FVector(-1000.0f, 1.0f, -500.0f);
+	const float decayConstant = 15.0f;
+
+	ArgusMath::ExponentialDecaySmoother<FVector> testValue = ArgusMath::ExponentialDecaySmoother<FVector>(vector0, decayConstant);
+
+#pragma region Test that the ExponentialDecaySmoother has the correct initial value.
+	TestEqual
+	(
+		FString::Printf
+		(
+			TEXT("[%s] Creating a %s, and testing that it has the correct initial value."),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(ArgusMath::ExponentialDecaySmoother<FVector>)
+		),
+		testValue.GetValue(),
+		vector0
+	);
+#pragma endregion
+
+	ChaseValue(testValue, vector0, vector1);
+
+#pragma region Test that smoothing from vec0 to vec1 over 1 second at 60 fps moves value sufficiently close to vec1.
+	TestTrue
+	(
+		FString::Printf
+		(
+			TEXT("[%s] Creating a %s, and testing that smoothing from {%f, %f, %f} to {%f, %f, %f} over 1 second at %f fps gets sufficiently close to target."),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(ArgusMath::ExponentialDecaySmoother<FVector>),
+			vector0.X, vector0.Y, vector0.Z,
+			vector1.X, vector1.Y, vector1.Z,
+			ArgusMathTests::k_framerate
+		),
+		testValue.GetValue().Equals(vector1, ArgusMathTests::k_valueTolerance)
+	);
+#pragma endregion
+
+	testValue.Reset(vector0);
+
+#pragma region Test that the ExponentialDecaySmoother has the correct value after reset.
+	TestTrue
+	(
+		FString::Printf
+		(
+			TEXT("[%s] Creating a %s, chasing a value, and test resetting back to the initial value."),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(ArgusMath::ExponentialDecaySmoother<FVector>)
+		),
+		testValue.GetValue().Equals(vector0, ArgusMathTests::k_valueTolerance)
+	);
+#pragma endregion
+
+	ChaseValue(testValue, vector0, vector2);
+
+#pragma region Test that smoothing from vec0 to vec2 over 1 second at 60 fps moves value sufficiently close to vec1.
+	TestTrue
+	(
+		FString::Printf
+		(
+			TEXT("[%s] Creating a %s, and testing that smoothing from {%f, %f, %f} to {%f, %f, %f} over 1 second at %f fps gets sufficiently close to target."),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(ArgusMath::ExponentialDecaySmoother<FVector>),
+			vector0.X, vector0.Y, vector0.Z,
+			vector2.X, vector2.Y, vector2.Z,
+			ArgusMathTests::k_framerate
+		),
+		testValue.GetValue().Equals(vector2, ArgusMathTests::k_valueTolerance)
+	);
+#pragma endregion
+
+	ChaseValue(testValue, vector2, vector0);
+
+#pragma region Test that smoothing from vec2 to vec0 over 1 second at 60 fps moves value sufficiently close to vec1.
+	TestTrue
+	(
+		FString::Printf
+		(
+			TEXT("[%s] Creating a %s, and testing that smoothing from {%f, %f, %f} to {%f, %f, %f} over 1 second at %f fps gets sufficiently close to target."),
+			ARGUS_FUNCNAME,
+			ARGUS_NAMEOF(ArgusMath::ExponentialDecaySmoother<FVector>),
+			vector2.X, vector2.Y, vector2.Z,
+			vector0.X, vector0.Y, vector0.Z,
+			ArgusMathTests::k_framerate
+		),
+		testValue.GetValue().Equals(vector0, ArgusMathTests::k_valueTolerance)
+	);
+#pragma endregion
+
+	return true;
+}
+
 #endif //WITH_AUTOMATION_TESTS
