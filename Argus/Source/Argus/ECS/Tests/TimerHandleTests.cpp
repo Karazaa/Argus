10fee956b8a453265ef555f9dcc4ae05440669a4
Karazaa <jamescolrepo@gmail.com>
2024-12-04 13:19:47 -0800

    Stubbed out new tests files.

---
+++ Argus/Source/Argus/ECS/Tests/TimerHandleTests.cpp
@@ -0,0 +1,9 @@
+// Copyright Karazaa. This is a part of an RTS project called Argus.
+
+#include "ArgusEntity.h"
+#include "ArgusTesting.h"
+#include "Misc/AutomationTest.h"
+
+#if WITH_AUTOMATION_TESTS
+
+#endif //WITH_AUTOMATION_TESTS
