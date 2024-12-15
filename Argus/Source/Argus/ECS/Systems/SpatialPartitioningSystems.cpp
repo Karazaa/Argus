402dce7fdc832f48498556c877f18384550c0b82
James Collins <jamescolrepo@gmail.com>
2024-12-15 13:53:49 -0800

    Starting edge retrieval

--- Argus/Source/Argus/ECS/Systems/SpatialPartitioningSystems.cpp
+++ Argus/Source/Argus/ECS/Systems/SpatialPartitioningSystems.cpp
@@ -1,6 +1,8 @@
 // Copyright Karazaa. This is a part of an RTS project called Argus.
 
 #include "SpatialPartitioningSystems.h"
+#include "ArgusLogging.h"
+#include "ArgusMacros.h"
 
 void SpatialPartitioningSystems::RunSystems(const ArgusEntity& spatialPartitioningEntity)
 {
@@ -31,6 +33,20 @@ void SpatialPartitioningSystems::RunSystems(const ArgusEntity& spatialPartitioni
 			continue;
 		}
 
+		SpatialPartitioningSystemsComponentArgs components;
+		components.m_entity = retrievedEntity;
+		components.m_transformComponent = transformComponent;
+		components.m_singletonSpatialParitioningComponent = spatialPartitioningComponent;
 		spatialPartitioningComponent->m_argusKDTree.InsertArgusEntityIntoKDTree(retrievedEntity);
 	}
+}
+
+bool SpatialPartitioningSystems::SpatialPartitioningSystemsComponentArgs::AreComponentsValidCheck(const WIDECHAR* functionName) const
+{
+	if (!m_entity || !m_singletonSpatialParitioningComponent)
+	{
+		ArgusLogging::LogInvalidComponentReferences(functionName, ARGUS_NAMEOF(SpatialPartitioningSystemsComponentArgs));
+		return false;
+	}
+	return true;
 }
--- Argus/Source/Argus/ECS/Systems/SpatialPartitioningSystems.h
+++ Argus/Source/Argus/ECS/Systems/SpatialPartitioningSystems.h
@@ -8,4 +8,16 @@ class SpatialPartitioningSystems
 {
 public:
 	static void RunSystems(const ArgusEntity& spatialPartitioningEntity);
+
+	struct SpatialPartitioningSystemsComponentArgs
+	{
+		ArgusEntity m_entity = ArgusEntity::k_emptyEntity;
+		const TransformComponent* m_transformComponent = nullptr;
+		SpatialPartitioningComponent* m_singletonSpatialParitioningComponent = nullptr;
+
+		bool AreComponentsValidCheck(const WIDECHAR* functionName) const;
+	};
+
+private:
+	static void PopulateRelevantNavEdges(const SpatialPartitioningSystemsComponentArgs& components);
 };
