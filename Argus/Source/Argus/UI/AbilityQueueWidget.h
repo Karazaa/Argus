00d7408e985113459f30f857c186aaa795618526
Karazaa <jamescolrepo@gmail.com>
2025-07-02 19:55:48 -0700

    Started populating AbilityQueueWidget with logic for showing ability icons. Blocked a bit by needing our spawn system to use an iterable queue.

--- Argus/Source/Argus/UI/AbilityQueueWidget.h
+++ Argus/Source/Argus/UI/AbilityQueueWidget.h
@@ -10,15 +10,25 @@ class UImage;
 class UUniformGridPanel;
 class UUniformGridSlot;
 
+UENUM(BlueprintType)
+enum class EAbilityQueueDataSource : uint8 
+{
+	SpawnQueue,
+	AbilityQueue
+};
+
 UCLASS()
 class UAbilityQueueWidget : public UArgusUserWidget
 {
 	GENERATED_BODY()
 
 public:
-	virtual void RefreshDisplay(const ArgusEntity& selectedEntity);
+	void RefreshDisplay(const ArgusEntity& selectedEntity);
 
 protected:
+	UPROPERTY(EditAnywhere)
+	EAbilityQueueDataSource m_abilityQueueDataSource = EAbilityQueueDataSource::SpawnQueue;
+
 	UPROPERTY(BlueprintReadWrite, Transient)
 	TObjectPtr<UUniformGridPanel> m_uniformGridPanel = nullptr;
 
@@ -28,6 +38,13 @@ protected:
 	UPROPERTY(EditDefaultsOnly)
 	FSlateBrush m_abilityImageSlateBrush;
 
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<UImage*> m_abilityIcons;
+
 	UPROPERTY(Transient)
 	TArray<UUniformGridSlot*> m_gridSlots;
+
+	void RefreshDisplayFromSpawnQueue(const ArgusEntity& selectedEntity);
+	void RefreshDisplayFromAbilityQueue(const ArgusEntity& selectedEntity);
+	void SetAbilityIcons(const TArray<uint32>& abilityRecordIds);
 };
